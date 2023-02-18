1705637dec6b8f7f5c17e995de2fb9f9941c5bde
Garrett Long WSL Ubuntu <garrettlong097@gmail.com>
2023-02-18 14:36:36 -0500

    test3

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -11,27 +11,27 @@ const args = minimist(process.argv.slice(2), {
 })
 if(args.help){
 	console.log(
-		`Usage: node-rps [SHOT]
-		Play Rock Paper Scissors (RPS)
+`Usage: node-rps [SHOT]
+Play Rock Paper Scissors (RPS)
 
-		  -h, --help      display this help message and exit
-		  -r, --rules     display the rules and exit
+  -h, --help      display this help message and exit
+  -r, --rules     display the rules and exit
 
-		Examples:
-		  node-rps        Return JSON with single player RPS result.
-				  e.g. {"player":"rock"}
-		  node-rps rock   Return JSON with results for RPS played against a simulated opponent.
-				  e.g {"player":"rock","opponent":"scissors","result":"win"}
+Examples:
+  node-rps        Return JSON with single player RPS result.
+		  e.g. {"player":"rock"}
+  node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+		  e.g {"player":"rock","opponent":"scissors","result":"win"}
 		`)
 	process.exit(0)
 }
 if(args.rules){
 	console.log(`
-		Rules for Rock Paper Scissors:
+Rules for Rock Paper Scissors:
 
-		  - Scissors CUTS Paper
-		  - Paper COVERS Rock
-		  - Rock CRUSHES Scissors
+  - Scissors CUTS Paper
+  - Paper COVERS Rock
+  - Rock CRUSHES Scissors
 	`)
 	process.exit(0)
 }
